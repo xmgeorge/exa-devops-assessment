0088f2af6a4a21a1555d84da33fae8035ab2cedc
George Ulahannan <george.ulahannan@gmail.com>
2024-07-04 16:51:02 +0100

    Added eks master

---
+++ tfscaffold/components/eks/master.tf
@@ -0,0 +1,60 @@
+
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = local.eks_cluster_name
+  role_arn = aws_iam_role.eks_master_iam_role.arn
+  version = var.cluster_version
+
+  vpc_config {
+    subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnets
+    endpoint_private_access = var.cluster_endpoint_private_access
+    endpoint_public_access  = var.cluster_endpoint_public_access
+    public_access_cidrs = var.cluster_public_access_cidrs
+  }
+
+  kubernetes_network_config {
+    service_ipv4_cidr = var.cluster_service_ipv4_cidr
+    ip_family = "ipv4"
+  }
+  enabled_cluster_log_types = ["api", "audit", "authenticator","controllerManager", "scheduler"]
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_AmazonEKSVPCResourceController,
+  ]
+
+  tags = local.tags
+}
+
+
+data "aws_iam_policy_document" "eks_master_assume_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
+
+resource "aws_iam_role" "eks_master_iam_role" {
+  name               = "${local.eks_cluster_name}-eks-master-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_master_assume_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_master_iam_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_master_iam_role.name
+}
