73d9ffe6221cc2b9a2a2ac20dcabf416f4235ce9
George Ulahannan <george.ulahannan@gmail.com>
2024-07-06 23:39:23 +0100

    argocdapp

---
+++ tfscaffold/components/argocdapp/main.tf
@@ -0,0 +1,32 @@
+
+resource "kubernetes_manifest" "sample_app" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+
+    metadata = {
+      name      = "sample-app"
+      namespace = "${data.terraform_remote_state.eksargocd.outputs.argocd_namespace}"
+    }
+
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/xmgeorge/sample-app.git"
+        targetRevision = "main"
+        path           = "k8s-manifests"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "default"
+      }
+      syncPolicy = {
+        automated = {
+          prune     = true
+          selfHeal  = true
+          allowEmpty = false
+        }
+      }
+    }
+  }
+}
--- tfscaffold/components/eks/outputs.tf
+++ tfscaffold/components/eks/outputs.tf
@@ -1,74 +1,89 @@
 output "eks_cluster_id" {
   description = "The ID of the EKS cluster."
+  sensitive = true
   value       = aws_eks_cluster.eks_cluster.id
 }
 
 output "eks_cluster_endpoint" {
   description = "Endpoint for your Kubernetes API server"
+  sensitive = true
   value       = aws_eks_cluster.eks_cluster.endpoint
 }
 
 output "kubeconfig_certificate_authority_data" {
   description = "Certificate data"
+  sensitive = true
   value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
 
 output "eks_cluster_arn" {
   description = "ARN of the cluster"
+  sensitive = true
   value       = aws_eks_cluster.eks_cluster.arn
 }
 
 output "eks_cluster_version" {
   description = "EKS version for the cluster."
+  sensitive = true
   value       = aws_eks_cluster.eks_cluster.version
 }
 
 output "eks_cluster_security_group_ids" {
   description = "List of security group IDs"
+  sensitive = true
   value       = aws_eks_cluster.eks_cluster.vpc_config[0].security_group_ids
 }
 
 output "eks_cluster_master_iam_role_arn" {
   description = "eks_cluster_master_iam_role_arn"
+  sensitive = true
   value       = aws_iam_role.eks_master_iam_role.arn
 }
 
 output "eks_cluster_master_iam_role_name" {
   description = "eks_cluster_master_iam_role_name"
+  sensitive = true
   value       = aws_iam_role.eks_master_iam_role.name
 }
 
 output "eks_cluster_oidc_issue_url" {
+  sensitive = true
   value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
 
 output "public_node_group_id" {
+  sensitive = true
   value = aws_eks_node_group.eks_nodegroup_public.id
 }
 
 output "public_node_group_status" {
+  sensitive = true
   value = aws_eks_node_group.eks_nodegroup_public.status
 }
 
 output "public_node_group_arn" {
+  sensitive = true
   value = aws_eks_node_group.eks_nodegroup_public.arn
 }
 
 output "public_node_group_version" {
+  sensitive = true
   value = aws_eks_node_group.eks_nodegroup_public.version
 }
 
 output "public_node_group_iam_role_arn" {
-
+  sensitive = true
   value = aws_iam_role.eks_nodegroup_iam_role.arn
 }
 
 output "openid_connect_provider_arn" {
+  sensitive = true
   description = "The OpenID Connect identity provider ARN"
   value       = aws_iam_openid_connect_provider.name.arn
 }
 
 output "openid_connect_provider_arn_extract" {
+  sensitive = true
   description = "The OpenID Connect identity provider ARN extract"
   value       = local.openid_connect_provider_arn_extract
 }
