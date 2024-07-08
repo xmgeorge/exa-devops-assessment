c1c7c4a3247af02d5962c8e523d705f70915d5ba
George Ulahannan <george.ulahannan@gmail.com>
2024-07-08 12:39:13 +0100

    testing colour

--- app/index.js
+++ app/index.js
@@ -1,9 +1,30 @@
-var os = require('os')
-var http = require('http')
+const os = require('os');
+const http = require('http');
 
-function handleRequest(req, res) {
-  res.write('Hi there! I\'m being served from ' + os.hostname())
-  res.end()
+// Function to generate a random color
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
-http.createServer(handleRequest).listen(3000)
+function handleRequest(req, res) {
+  const color = getRandomColor();
+  const hostname = os.hostname();
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(`
+    <html>
+      <body style="background-color: ${color};">
+        <h1 style="color: white;">Hi there! I'm being served from ${hostname}</h1>
+      </body>
+    </html>
+  `);
+  res.end();
+}
+
+http.createServer(handleRequest).listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
